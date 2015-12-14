3d9391a199181c697881da514e2264c94bed9ac0
johnkinzel <john.kinzel426@gmail.com>
2015-12-14 17:29:54 -0600

    Finished
    Finished

--- SchedulingAlgorithms/FirstComeFirstServe/main.cpp
+++ SchedulingAlgorithms/FirstComeFirstServe/main.cpp
@@ -4,7 +4,6 @@
 //Player will enter in burst time and the program will compute the wait time for each process, the average wait time
 
 #include <iostream>
-#include <string>
 
 
 int main()
@@ -15,8 +14,11 @@ int main()
 
 	int jobExe[4], jobWait[4], jobServe[4]; //the burst time, wait time, and service time respectivley
 
+	float avgWait = 0;
 
-	int test;
+	float sum = 0;
+
+	int temp = 0;
 
 	/////////////////////////////////////////
 	//Input/output block
@@ -50,16 +52,46 @@ int main()
 	jobServe[0] = 0;
 	/////////////////////////////////////////////////////
 
-	for (int i = 1; i <= 4; i++)//calculate service time
+
+	////////////////calculate service time///////////////
+	for (int i = 1; i < 4; i++)
 	{
+		jobServe[i] = jobExe[i - 1] + jobServe[i - 1];
+
+	}
+
+	////////////////calculate wait time/////////////////
+
+	for (int i = 1; i < 4; i++)
+	{
+		jobWait[i] = jobServe[i] - (i);
 
 
 	}
 	
 
+
+	///////////////////calculate avg wait time///////
+
+
+	sum = jobWait[0] + jobWait[1] + jobWait[2] + jobWait[3];
 	
+	avgWait = sum / 4;
+
+	//////////////OUTPUT Y'ALL////////////////
+
+	std::cout << "Process Number" << "\t" << "Burst Time" << "\t" << "Waiting Time " << "\t" << "Service Time\n";
+
+
+	for (int i = 0; i < 4; i++)
+	{
+		std::cout << i + 1 << "  \t\t" << jobExe[i] <<  "  \t\t" << jobWait[i] << "  \t\t" << jobServe[i] << "\n";
+
+	}
 	
-	
-	system("Pause");
+	std::cout << "Average Wait Time was " << avgWait << std::endl << "Thanks! See you later!\n\n";
+
+
+
 	return 0;
 }
